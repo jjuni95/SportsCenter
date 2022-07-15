e7b88ceeceea72e5b8d9a276bc055962638449f4
jjuni <wnsgml6440@naver.com>
2022-07-16 00:14:08 +0900

    UserVo 멤버변수명 phone -> phoneNumber 로 수정

--- src/main/java/com/proj/sportscenter/vo/UserVo.java
+++ src/main/java/com/proj/sportscenter/vo/UserVo.java
@@ -21,7 +21,7 @@ public class UserVo {
 	private String userId;
 	private String userName;
 	private String password;
-	private String phone;
+	private String phoneNumber;
 	private String gender;
 	private LocalDateTime regDate;
 	
