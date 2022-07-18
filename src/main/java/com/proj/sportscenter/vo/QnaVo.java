9ac27cd577bab7c13feb578e115bc40e8ee4717e
drunkenele <drunkenele@gmail.com>
2022-07-18 11:42:26 +0900

    각 페이지별 Vo 제작

---
+++ src/main/java/com/proj/sportscenter/vo/QnaVo.java
@@ -0,0 +1,25 @@
+package com.proj.sportscenter.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter //이거 두개가 귀찮다? 그럼 @Data로 쓴다.
+@Setter //
+@NoArgsConstructor //기본생성자 만들어줌 (필수!)
+@AllArgsConstructor //파라미터가 들어있는 생성자
+@EqualsAndHashCode //필수 vo 객체 안에 데이터가 동일한 경우 같은 객체로 인식
+@ToString
+
+public class QnaVo {
+	private Long userNo; //관습상 long으로한다~.
+	private String userId;
+	private String userName;
+	private LocalDateTime regDate;
+	private String noticeList;
+	private String noticeContent;
+}
